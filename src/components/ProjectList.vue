cd5c8bffc69910bb256801e2e75dd070c1007c9a
Dalton Kirk <daltonkirk5@gmail.com>
2021-05-29 12:22:26 +0100

    added github link and logo to project cards

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,13 +1,22 @@
 <template>
-    <ul class="project-list">
-        <li v-for="project in $static.projects.edges" :key="project.node.title">
-            <a :href="`/projects/${project.node.slug}`">
-                <h3>{{project.node.title}}</h3>
-                <p>{{ project.node.description }}</p>
-            </a>
-        </li>
-    </ul>
-</template>
+  <ul class="project-list">
+    <li v-for="project in $static.projects.edges" :key="project.node.title">
+      <a class="project-list__link" :href="`/projects/${project.node.slug}`">
+        <h3>{{ project.node.title }}</h3>
+        <p>{{ project.node.description }}</p>
+        <a
+          class="project-list__github"
+          v-if="project.node.github"
+          :href="project.node.github"
+          target="_blank"
+          rel="noopener"
+        >
+          <g-image src="~/assets/images/GitHub-Mark-32px.png" alt="github logo" width="32" />
+        </a>
+      </a>
+    </li>
+  </ul> </template
+>@
 
 <static-query>
   query {
@@ -16,7 +25,8 @@
        node {
          slug,
          title,
-         description
+         description,
+         github
        }
      }
    }
@@ -25,36 +35,41 @@
 
 <script>
 export default {
- name: 'ProjectList'
-}
+  name: 'ProjectList',
+};
 </script>
 
 <style lang="scss">
-    .project-list {
-        flex-wrap: wrap;
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
+.project-list {
+  flex-wrap: wrap;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
 
-        li {
-            box-shadow: 0 0 6px 3px rgba(0, 0, 0, .15);
-            border-radius: 5px;
-            margin-bottom: 1rem;
+  li {
+    box-shadow: 0 0 6px 3px rgba(0, 0, 0, 0.15);
+    border-radius: 5px;
+    margin-bottom: 1rem;
 
-            h3 {
-                margin: 0 0 20px 0;
-            }
-
-            a {
-                padding: 20px;
-                display: block;
-                color: #000;
-                height: 100%;
-                text-decoration: none;
-                &:active {
-                    color: grey;
-                }
-            }
-        }
+    h3 {
+      margin: 0 0 20px 0;
     }
+  }
+}
+
+.project-list__link {
+  padding: 20px;
+  display: block;
+  color: #000;
+  height: 100%;
+  text-decoration: none;
+
+  &:active {
+    color: grey;
+  }
+}
+
+.project-list__github {
+  padding: 0;
+}
 </style>
