41c0b8c61fe2d35479e688d110f2c66ea209f9ff
Dalton Kirk <daltonkirk5@gmail.com>
2021-09-11 15:17:24 +0100

    Added pixel art animation project

---
+++ src/pages/projects/PixelArtAnimator.vue
@@ -0,0 +1,27 @@
+<template>
+  <Layout>
+    <h1>Pixel Art Animator</h1>
+    <div>
+        <a href="https://animate.daltonkirk.com" target="_blank" rel="noopener">Try it out.</a>
+      <p></p>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Project'
+  }
+}
+</script>
+
+<style>
+  p + h2 {
+    margin-bottom: -20px;
+  }
+
+  iframe {
+    border: none;
+  }
+</style>
